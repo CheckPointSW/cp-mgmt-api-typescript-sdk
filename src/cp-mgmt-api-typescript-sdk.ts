6df0b269445b8b481da2a3848e83067112e70377
orso <orso@checkpoint.com>
2021-06-15 17:21:46 +0300

    Change package name and export ApiClient as default.

--- src/cp-mgmt-api-typescript-sdk.ts
+++ src/cp-mgmt-api-typescript-sdk.ts
@@ -1,7 +1,10 @@
+import ApiClient from "./cpapi/apiClient";
+
 export * from './cpapi/apiClient';
 export {default as ApiUtils} from './cpapi/apiUtils';
 export {default as log} from './decorators/loggingDecorator';
 export {default as getLogger} from './logger/log';
 export {default as CustomLogger} from './logger/customLogger';
 export {default as ApiError} from './errors/apiError';
+export default ApiClient;
 
